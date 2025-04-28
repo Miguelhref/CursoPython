ceac708bf9aa8ba4ea2e6aefce7f51b35d4bd9d4
miguelhref <miguelhref@gmail.com>
2025-04-28 12:06:41 +0200

    coolmod listo

--- scraping1/actividad1/coolmod.py
+++ scraping1/actividad1/coolmod.py
@@ -5,37 +5,76 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+import re
+def scrap_coolmod(keyword, pages, json_file):
+    results = []
+    seen_skus = set()  # <--- nuevo
+    opts = Options()
+    opts.add_argument("user-agent=...")
+    opts.add_argument("--window-position=1100,0")
+    opts.add_experimental_option("excludeSwitches", ["enable-automation"])
+    opts.add_experimental_option('useAutomationExtension', False)
 
-#def scrap_deporte_outlet(keyword, pages, json_file):
-keyword = "monitor"
-pages = 3
-json_file = f'{keyword}_coolmod'
-results = []
-opts = Options()
-opts.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.6312.86 Safari/537.36")
-opts.add_argument("--window-position=1100,0")
-opts.add_experimental_option("excludeSwitches", ["enable-automation"])
-opts.add_experimental_option('useAutomationExtension', False)
-#opts.add_argument("--headless")  
-
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=opts)
-
-try:
-
-    driver.get('https://www.coolmod.com/')
-    sleep(3)
-
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=opts)
 
     try:
-        driver.find_element(By.ID, "CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll").click()
-        sleep(5)
-    except:
-        pass
+        driver.get('https://www.coolmod.com/')
+        sleep(3)
+        
+        try:
+            driver.find_element(By.ID, "CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll").click()
+            sleep(5)
+        except:
+            pass
 
-    infinite_pages = (pages == 0)
-    i = 1
-    while (infinite_pages or i<=pages):
+        infinite_pages = (pages == 0)
+        i = 1
+        while (infinite_pages or i<=pages):
 
-        url = f'https://www.coolmod.com/?gad_source=1&gbraid=0AAAAAD_BC_PFg4lIqxGCYG2kCslqo3iDD&gclid=Cj0KCQjw5azABhD1ARIsAA0WFUF8wM_dzdNxl9yPD8cjbidkn6sY3jUmuV8M-Vp4xtJmuxnrerRz3P4aAr0iEALw_wcB#01cc/fullscreen/m=and&p=4&q=monitor'
-except Exception as e:
-    print()
+            url = f'http://coolmod.com/#01cc/fullscreen/m=and&p={i}&q={keyword}'
+            driver.get(url)
+            sleep(2)
+            elements = driver.find_elements(By.CLASS_NAME,'dfd-card-preset-product')
+            for element in elements:
+                image_url = element.find_element(By.TAG_NAME,'img').get_attribute('src')
+                
+                sku_raw = element.find_element(By.CLASS_NAME,'dfd-cart-add-button')
+                sku_button = sku_raw.find_element(By.TAG_NAME,'button')
+                sku_raw_attr = sku_button.get_attribute('data-item')
+                sku_match = re.search(r'PROD-(\d+)', sku_raw_attr)
+                
+                sku = sku_match.group(1)
+                
+                if sku in seen_skus:
+                    continue  # <--- no guardar duplicados
+                seen_skus.add(sku)
+
+                item_url = element.find_element(By.TAG_NAME,'a').get_attribute('href')
+                name = element.find_element(By.CLASS_NAME,'dfd-card-title').get_attribute('title')
+                try:
+                    element_price = element.find_element(By.CLASS_NAME,'dfd-card-pricing')
+                    pvp = element_price.find_elements(By.CLASS_NAME,'dfd-card-price')[1].text.replace(".","").replace(",",".").replace("€","")
+                    price = element_price.find_element(By.CLASS_NAME,'dfd-card-price--sale').text.replace(".","").replace(",",".").replace("€","")
+                except Exception:
+                    element_price = element.find_element(By.CLASS_NAME,'dfd-card-pricing')
+                    price = element_price.find_element(By.CLASS_NAME,'dfd-card-price').text.replace(".","").replace(",",".").replace("€","")
+                    pvp = None
+                    
+                results.append({
+                    "sku": sku,
+                    "image_url": image_url,
+                    "item_url": item_url,
+                    "name": name,
+                    "price": price,
+                    "pvp": pvp
+                })
+            i += 1
+
+    except Exception as e:
+        print('Error', e)
+
+    with open(json_file + ".json", "w", encoding="utf-8") as f:
+    
+        json.dump(results, f, ensure_ascii=False, indent=2)
+
+    print("Número de productos encontrados en coolmod:", len(results))
--- scraping1/actividad1/keyword.py
+++ scraping1/actividad1/keyword.py
@@ -4,9 +4,10 @@ from pullandbear import scrap_pullandbear
 from glisshop import scrap_glisshop
 from electroprecio import scrap_electroprecio
 from deporte_outlet import scrap_deporte_outlet
+from coolmod import scrap_coolmod
 import threading
 
-keyword = 'nike'
+keyword = 'monitor'
 
 #carrefour.search(keyword)
 #scrap_lefties(archivo_json="sudaderas_lefties",keyword=keyword)
@@ -21,11 +22,14 @@ def run_electroprecio():
 def run_deporte_outlet():
     scrap_deporte_outlet(keyword, 3, f"{keyword}_deporte_outlet")
 
+def run_coolmod():
+    scrap_coolmod(keyword, 3, f"{keyword}_coolmod")
 # Crear los hilos
 threads = [
-   # threading.Thread(target=run_glisshop),
-   # threading.Thread(target=run_electroprecio),
-    threading.Thread(target=run_deporte_outlet),
+   # threading.Thread(target=run_glisshop), revisar
+   # threading.Thread(target=run_electroprecio), revisar
+   # threading.Thread(target=run_deporte_outlet),
+    threading.Thread(target=run_coolmod()),
 ]
 
 #iniciar los hilos
